5f25e78bfe8dddd86f7f5e50f0939bac00c5dea6
bongsik <minmediaservice@gmail.com>
2024-01-18 14:19:47 +0900

    [ADD] 24/01/16 JDBC 3일차 - pstmt 적용
    1. Statment가 가지는 보안적 한계를 보완하는 PreparedStatement 사용
    2. Statement와 PreparedStatement의 차이점을 정리
     2.1 위치홀더 사용
     2.2 쿼리문을 미리 컴파일 함
     2.3 위치홀더 값을 셋팅함
     2.4 실행하는 메소드에 전달값이 필요없음.

---
+++ src/com/kh/jdbc/day03/stmt/employee/run/EmployeeRun.java
@@ -0,0 +1,13 @@
+package com.kh.jdbc.day03.stmt.employee.run;
+
+import com.kh.jdbc.day03.stmt.employee.view.EmployeeView;
+
+public class EmployeeRun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EmployeeView view = new EmployeeView();
+		view.startProgram();
+	}
+
+}
